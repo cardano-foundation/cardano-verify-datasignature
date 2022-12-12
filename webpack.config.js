9bf7bf46453b3d1ee777517b5934a9551136b89d
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2022-12-12 20:54:53 +0100

    feat: add webpack to build the module for browser and commonjs

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: {
+      name: 'CardanoVerifyDataSignature',
+      type: 'var',
+      export: 'default',
+    },
+    libraryTarget: 'umd',
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-typescript', { allowNamespaces: true }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.scss'],
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+    },
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  infrastructureLogging: {
+    level: 'info',
+  },
+};
+
+module.exports = config;
