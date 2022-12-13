e2e5341aa382a6ac4e0fd98566904c10599fc968
Fabian Bormann <fabian.bormann@cardanofoundation.org>
2022-12-13 14:16:41 +0100

    feat: add payment address verification

--- index.test.ts
+++ index.test.ts
@@ -33,7 +33,7 @@ describe('testing signature verification', () => {
     expect(verifySignature(signature, key, message, address)).toBe(false);
   });
 
-  test('signature verification using cip30 datasignature with message check should fail.', () => {
+  test('signature verification using cip30 datasignature with message check should fail', () => {
     const key =
       'a4010103272006215820b89526fd6bf4ba737c55ea90670d16a27f8de6cc1982349b3b676705a2f420c6';
     const signature =
@@ -43,7 +43,7 @@ describe('testing signature verification', () => {
     expect(verifySignature(signature, key, message)).toBe(false);
   });
 
-  test('signature verification using cip30 datasignature with address check should fail.', () => {
+  test('signature verification using cip30 datasignature with address check should fail', () => {
     const address =
       'stake_test1uyvfslqkzgrf6syq5r4jg7pqewv8l65phh024lw5r7vk9qgznhyty';
     const key =
@@ -52,5 +52,16 @@ describe('testing signature verification', () => {
       '84582aa201276761646472657373581de118987c1612069d4080a0eb247820cb987fea81bddeaafdd41f996281a166686173686564f458264175677573746120416461204b696e672c20436f756e74657373206f66204c6f76656c61636558401712458b19f606b322982f6290c78529a235b56c0f1cec4f24b12a8660b40cd37f4c5440a465754089c462ed4b0d613bffaee3d1833516569fda4852f42a4a0f';
 
     expect(verifySignature(signature, key, undefined, address)).toBe(false);
+  });
+
+  test('signature verification with payment address', () => {
+    const address =
+      'addr1qxtu4w2rq2mdguw4fkms2ge4m070nq8cmlyjfhghwlh8sjscnp7pvysxn4qgpg8ty3uzpjuc0l4gr0w74t7ag8uev2qseuyw6u';
+    const key =
+      'a4010103272006215820b89526fd6bf4ba737c55ea90670d16a27f8de6cc1982349b3b676705a2f420c6';
+    const signature =
+      '84582aa201276761646472657373581de118987c1612069d4080a0eb247820cb987fea81bddeaafdd41f996281a166686173686564f458264175677573746120416461204b696e672c20436f756e74657373206f66204c6f76656c61636558401712458b19f606b322982f6290c78529a235b56c0f1cec4f24b12a8660b40cd37f4c5440a465754089c462ed4b0d613bffaee3d1833516569fda4852f42a4a0f';
+
+    expect(verifySignature(signature, key, undefined, address)).toBe(true);
   });
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,37 @@
 import { Bip32PublicKey } from '@stricahq/bip32ed25519';
 import { getPublicKeyFromCoseKey, CoseSign1 } from '@stricahq/cip08';
 import { Decoder } from '@stricahq/cbors';
-import { RewardAddress } from '@stricahq/typhonjs/dist/address';
+import { BaseAddress, RewardAddress } from '@stricahq/typhonjs/dist/address';
+import { utils } from '@stricahq/typhonjs';
 
+const Network = {
+  MAINNET: 1,
+  TESTNET: 0,
+};
+
+/**
+ * This method can be used to verify a cip30 data signature.
+ *
+ * https://cips.cardano.org/cips/cip30/#datasignature
+ * type DataSignature = {
+ *   signature: cbor\<COSE_Sign1>,
+ *   key: cbor\<COSE_Key>,
+ * };
+ *
+ * Optional:
+ *
+ * Furthermore a plain text message can be provided to check if both
+ * the plain text and the signed message are equal
+ *
+ * Also a payment (addr1/addr_test1) or stake address (stake1/stake_test1) can be provided
+ * to verify that this readable address belongs to the signature
+ *
+ * @param {string} signature - A COSE_Sign1 cbor string.
+ * @param {string} key - A COSE_Key cbor string
+ * @param {string} message - An optional plain text message
+ * @param {string} address - An optional address starting with (addr1/addr_test1/stake1/stake_test1)
+ * @returns {string} boolean - True if the verification was successful otherwise false
+ */
 const verifySignature = (
   signature: string,
   key: string,
@@ -21,14 +50,35 @@ const verifySignature = (
   }
 
   if (address) {
+    let providedRewardAddress = address;
+    let network = Network.MAINNET;
+
+    if (address.startsWith('addr1')) {
+      const paymentAddress = utils.getAddressFromBech32(address) as BaseAddress;
+
+      providedRewardAddress = new RewardAddress(
+        Network.MAINNET,
+        paymentAddress.stakeCredential
+      ).getBech32();
+    } else if (address.startsWith('stake_test1')) {
+      network = Network.TESTNET;
+    } else if (address.startsWith('addr_test1')) {
+      network = Network.TESTNET;
+      const paymentAddress = utils.getAddressFromBech32(address) as BaseAddress;
+      providedRewardAddress = new RewardAddress(
+        Network.MAINNET,
+        paymentAddress.stakeCredential
+      ).getBech32();
+    }
+
     const coseSign1PublicKey = new Bip32PublicKey(publicKeyBuffer);
     const credential = {
       hash: coseSign1PublicKey.toPublicKey().hash().toString('hex'),
       type: 0,
     };
 
-    const rewardAddress = new RewardAddress(1, credential);
-    if (rewardAddress.getBech32() !== address) {
+    const rewardAddress = new RewardAddress(network, credential);
+    if (rewardAddress.getBech32() !== providedRewardAddress) {
       return false;
     }
   }
